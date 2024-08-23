09200ebdd8d3238eb8ca508f4828c7b7bcc652bd
Federico Veiga HP <fede.veiga14@gmail.com>
2024-08-23 20:08:36 -0300

    add show profesiones

--- resources/views/admin/profesiones/index.blade.php
+++ resources/views/admin/profesiones/index.blade.php
@@ -82,10 +82,7 @@
 @stop
 
 @section('js')   
-<script src="{{ asset('admin/profesionales/index.js') }}"> </script>
+<script src="{{ asset('admin1/profesiones/index.js') }}"> </script>
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
   
 @stop
