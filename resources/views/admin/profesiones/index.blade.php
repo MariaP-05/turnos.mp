728e05f2569e55a372ce7593151721d1e3c82a07
Federico Veiga HP <fede.veiga14@gmail.com>
2024-08-23 21:20:37 -0300

    add buscador fechas en turnos

--- resources/views/admin/profesiones/index.blade.php
+++ resources/views/admin/profesiones/index.blade.php
@@ -78,11 +78,16 @@
 @stop
 
 @section('css')   
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
 
 @stop
 
 @section('js')   
+
 <script src="{{ asset('admin1/profesiones/index.js') }}"> </script>
 
-  
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+   
 @stop
