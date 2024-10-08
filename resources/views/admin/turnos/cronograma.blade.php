de33560c625a2dcef6d7fe0b56a8da89d74faec0
Federico Veiga HP <fede.veiga14@gmail.com>
2024-10-08 17:00:45 -0300

    fix js cronograma

--- resources/views/admin/turnos/cronograma.blade.php
+++ resources/views/admin/turnos/cronograma.blade.php
@@ -74,14 +74,17 @@
 
     @section('js')
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 
+        <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"> </script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"> </script>
+        
 
     <script>
         $(document).ready(function() {
@@ -114,14 +117,13 @@
                     },
                     lengthChange: false,
                     pageLength: 60,
-                    order: [
-                        [0, 'asc']
-                    ]
-                    
-                }
+                    responsive: true,
+               autowith:false ,
+               order: [[ 0, 'asc' ]]
+                    }
 
-            );
-
+                );
+                   
 
         });
 
